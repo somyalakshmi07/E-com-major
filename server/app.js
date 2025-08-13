48fd72a1fac71d201c14d8680727a61ea358be28
somyalakshmi07 <somyalakshmi18@gmail.com>
2025-08-13 18:00:04 +0530

    port changed to 8080

--- server/app.js
+++ server/app.js
@@ -76,7 +76,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`
   Server running in ${process.env.NODE_ENV || 'development'} mode
